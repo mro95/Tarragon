c8d2146a871d32ff71af536f2054131bdd393c69
Maarten Oosting <maarten.tutoworld@gmail.com>
2016-02-05 22:38:55 +0100

    Add stdin support

--- tarragon.js
+++ tarragon.js
@@ -2,26 +2,47 @@ var fs = require('fs');
 var jsdom = require('jsdom');
 // TODO: callbackify this to make it more error prone
 // TODO: validate and check arguments
-var contents = fs.readFileSync(process.argv[2], 'utf8');
 
-// TODO: figure out a better way to dump html
-// (preferably with doctype)
-jsdom.env(contents, function (err, window) {
-  var document = window.document;
+var contents = '';
 
-  var $ = document.querySelector.bind(document);
-  var $$ = document.querySelectorAll.bind(document);
-  var elems = $$('link[type="text/data"]');
+if(process.argv[2]) {
+    contents = fs.readFileSync(process.argv[2], 'utf8');
+    parse(contents);
+}
+process.stdin.setEncoding('utf8');
 
-  Array.prototype.forEach.call(elems, function(elem) {
-    // TODO: callbackify this to make it more error prone
-    var data = JSON.parse(fs.readFileSync(elem.href, 'utf-8'));
-
-    for (selector in data) {
-      var body = data[selector];
-      $(selector).innerHTML = body;
+process.stdin.on('readable', () => {
+    var chunk = process.stdin.read();
+    if (chunk !== null) {
+        //process.stdout.write(`data: ${chunk}`);
+        contents += chunk.toString();
     }
-  });
-
-  console.log(document.documentElement.outerHTML);
 });
+
+process.stdin.on('end', () => {
+    parse(contents)
+});
+
+function parse(contents) {
+    // TODO: figure out a better way to dump html
+    // (preferably with doctype)
+    jsdom.env(contents, function (err, window) {
+      var document = window.document;
+
+      var $ = document.querySelector.bind(document);
+      var $$ = document.querySelectorAll.bind(document);
+      var elems = $$('link[type="text/data"]');
+
+      Array.prototype.forEach.call(elems, function(elem) {
+        // TODO: callbackify this to make it more error prone
+        var data = JSON.parse(fs.readFileSync(elem.href, 'utf-8'));
+
+        for (selector in data) {
+          var body = data[selector];
+          $(selector).innerHTML = body;
+        }
+      });
+
+      console.log(document.documentElement.outerHTML);
+    });
+}
